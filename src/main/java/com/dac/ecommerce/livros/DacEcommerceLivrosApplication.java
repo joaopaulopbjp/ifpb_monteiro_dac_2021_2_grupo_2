b7300e5c45b75fb7fd425217be16b6335a7e6ff1
Igor <igor.pimentel.msi@hotmail.com>
2021-09-23 14:41:49 -0300

    Estrutura do Projeto

---
+++ src/main/java/com/dac/ecommerce/livros/DacEcommerceLivrosApplication.java
@@ -0,0 +1,23 @@
+package com.dac.ecommerce.livros;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DacEcommerceLivrosApplication implements CommandLineRunner {
+	
+	public DacEcommerceLivrosApplication() {
+		
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DacEcommerceLivrosApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+	
+}
