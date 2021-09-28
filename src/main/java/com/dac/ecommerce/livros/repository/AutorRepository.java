e24004343df2911787d833d7bfa79c8a911d4ea7
ezzequiias <86009508+ezzequiias@users.noreply.github.com>
2021-09-28 17:17:47 -0300

    # WARNING: head commit changed in the meantime
    Criado Autor Repository / Criado AutorService

--- src/main/java/com/dac/ecommerce/livros/repository/AutorRepository.java
+++ src/main/java/com/dac/ecommerce/livros/repository/AutorRepository.java
@@ -1,4 +1,6 @@
 package com.dac.ecommerce.livros.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.dac.ecommerce.livros.model.Autor;
@@ -6,6 +8,10 @@ import com.dac.ecommerce.livros.model.Autor;
 @Repository
 public interface AutorRepository extends JpaRepository<Autor, Long>{
 	
+	public Autor findUniqueByNome(String nome);
 	
+	public List<Autor> findByNome(String nome);
+	
+	public Autor findByID(long ID);
 
 }
