cc29017d872a71ea814c1f5bf8df465914133e9a
ezzequiias <86009508+ezzequiias@users.noreply.github.com>
2021-09-29 08:05:31 -0300

    CRUD AUTOR

--- src/main/java/com/dac/ecommerce/livros/services/AutorService.java
+++ src/main/java/com/dac/ecommerce/livros/services/AutorService.java
@@ -1,5 +1,7 @@
 package com.dac.ecommerce.livros.services;
 import java.util.List;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.dac.ecommerce.livros.model.Autor;
@@ -32,4 +34,23 @@ public class AutorService {
 	public Autor recuperarAutor(Long id) {
 		return repository.getById(id);
 	}
-}
+	
+	public void remove(long ID) {
+		repository.deleteById(ID);
+	}
+	
+	public Autor pesquisarAutorPorNome(String nome) {
+		Autor autor = repository.findUniqueByNome(nome);
+		return autor;
+	}
+	
+	public void editarAutor(Autor novoAutor, long idAntigo) {
+		Autor autorSalvo = repository.findByID(idAntigo);
+		BeanUtils.copyProperties(novoAutor, autorSalvo);
+		repository.save(autorSalvo);
+	}
+	
+	
+	
+	}
+
