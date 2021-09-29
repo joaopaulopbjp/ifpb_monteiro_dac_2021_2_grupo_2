1d525879ee35d1df8ca52cc9b89f47c43b0089cc
luiz <luiz@DESKTOP-JTN0JTR>
2021-09-28 21:22:18 -0300

    adicionando as classes Estoque, Pedido e PedidoItem.

--- src/main/java/com/dac/ecommerce/livros/repository/AutorRepository.java
+++ src/main/java/com/dac/ecommerce/livros/repository/AutorRepository.java
@@ -1,6 +1,6 @@
 package com.dac.ecommerce.livros.repository;
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.dac.ecommerce.livros.model.Autor;
@@ -12,6 +12,6 @@ public interface AutorRepository extends JpaRepository<Autor, Long>{
 	
 	public List<Autor> findByNome(String nome);
 	
-	public Autor findByID(long ID);
+	public Optional<Autor> findById(Long ID);
 
 }
---
+++ src/main/java/com/dac/ecommerce/livros/repository/EstoqueRepository.java
@@ -0,0 +1,11 @@
+package com.dac.ecommerce.livros.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.dac.ecommerce.livros.model.Estoque;
+
+@Repository
+public interface EstoqueRepository extends JpaRepository<Estoque, Integer>{
+
+	
+	
+}
