70dcb83fd2e8b9f01cdc04d6867fa5bc0f297923
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2021-10-05 11:09:13 -0300

    Remoção de métodos setters e getters da classe endereço

--- src/main/java/com/dac/ecommerce/livros/model/pedido/Endereco.java
+++ src/main/java/com/dac/ecommerce/livros/model/pedido/Endereco.java
@@ -8,62 +8,6 @@ import lombok.Data;
 @Embeddable
 public class Endereco {
 	
-	public String getCep() {
-		return cep;
-	}
-
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
-
-	public Integer getNumero() {
-		return numero;
-	}
-
-	public void setNumero(Integer numero) {
-		this.numero = numero;
-	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-
-	public String getEstado() {
-		return estado;
-	}
-
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-
-	public String getBairro() {
-		return bairro;
-	}
-
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-
-	public String getRua() {
-		return rua;
-	}
-
-	public void setRua(String rua) {
-		this.rua = rua;
-	}
-
-	public String getComplemento() {
-		return complemento;
-	}
-
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
-
 	private String cep;
 	
 	private Integer numero;
