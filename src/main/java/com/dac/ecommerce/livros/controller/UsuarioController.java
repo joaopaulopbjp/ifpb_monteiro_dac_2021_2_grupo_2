f5d331e34bae1869bbf72c25045c24d29e3765f4
ezzequiias <86009508+ezzequiias@users.noreply.github.com>
2021-11-29 09:41:57 -0300

    GET e POST Mapping de Cadastrar/Adicionar usuário criados na controller

--- src/main/java/com/dac/ecommerce/livros/controller/UsuarioController.java
+++ src/main/java/com/dac/ecommerce/livros/controller/UsuarioController.java
@@ -1,13 +1,48 @@
 package com.dac.ecommerce.livros.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dac.ecommerce.livros.model.user.Usuario;
+import com.dac.ecommerce.livros.services.UsuarioService;
 
 
 @Controller
 @RequestMapping("/user")
 public class UsuarioController {
+	
+	@Autowired
+	UsuarioService usuarioService;
+	
+	@GetMapping("/cadastrar-usuario") 
+	public String cadastrarUsuario(Model model) {
+		
+		model.addAttribute("usuario", new Usuario());
+		return "usuario/login_user";
+		
+	}
+	
+	@PostMapping("/adicionar-usuario") 
+	public String cadastrarUsuario(@ModelAttribute("usuario") Usuario usuario, BindingResult result, Model modelo) throws Exception {
+		
+		if(!result.hasErrors()) {
+			
+			usuarioService.save(usuario);
+			
+			System.out.println("Post: cadastrado com sucesso");
+
+			return "redirect:/home";
+		}
+		
+		return "/usuario/login_user";
+		
+	}
 
 	@GetMapping("/menu-conta")
 	public String menuConta() {
